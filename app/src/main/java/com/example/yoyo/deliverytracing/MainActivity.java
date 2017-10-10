ca49b64b55f201ff9c3d243e48d00f5ae537cf8c
LuisCastanho <luis95castanho@gmail.com>
2017-10-10 12:18:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.yoyo.deliverytracing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
+    }
+}
