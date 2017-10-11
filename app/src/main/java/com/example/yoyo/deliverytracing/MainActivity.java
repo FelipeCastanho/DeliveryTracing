ab7c3ffb0b8ff149ebddcb9c4142fcc4c8678b35
LuisCastanho <luis95castanho@gmail.com>
2017-10-10 23:33:04 -0500

    Primera pantalla sin clics

--- app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.yoyo.deliverytracing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
