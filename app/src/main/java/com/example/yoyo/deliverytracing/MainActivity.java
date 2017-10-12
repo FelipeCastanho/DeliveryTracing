d7477df08367fd390e3fb47411616e7dae866bd7
LuisCastanho <luis95castanho@gmail.com>
2017-10-11 20:20:56 -0500

    Primera pantalla con Listener

--- app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.yoyo.deliverytracing;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -17,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
+    }
+
+    public void buttonTapped(View view) {
+
+        int numId = view.getId();
+        String id = view.getResources().getResourceEntryName(numId);
+        if(id.equals("rastrear")){
+            Intent newActivity = new Intent(MainActivity.this,RastreoActivity.class);
+                    //.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);//Con esta linea se mueren las actividades (No la uso aqui porque no es necesario, solo sera necesario despues de iniciar sesion)
+            startActivity(newActivity );
+        }
+        else if(id.equals("login")){
+
+        }
+
     }
 }
