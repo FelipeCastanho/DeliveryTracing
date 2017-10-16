e8fa364d28e2fbe1cae679c2f037e3c3fee69abf
konaizumi <alejita-81@hotmail.com>
2017-10-15 20:20:16 -0500

    Vista Registro :')

--- app/src/main/java/com/example/yoyo/deliverytracing/LoginActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.yoyo.deliverytracing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView registrame = (TextView) findViewById(R.id.registrarmeText);
+        registrame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentRegistro = new Intent(LoginActivity.this, RegistroActivity.class);
+                LoginActivity.this.startActivity(intentRegistro);
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/example/yoyo/deliverytracing/RegistroActivity.java
@@ -0,0 +1,29 @@
+package com.example.yoyo.deliverytracing;
+
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+
+        Switch isAdmin = (Switch) findViewById(R.id.adminSwitch);
+        isAdmin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextInputLayout empresa = (TextInputLayout) findViewById(R.id.textInputLayoutEmpresa);
+                if(isChecked){
+                    empresa.setVisibility(View.VISIBLE);
+                }else{
+                    empresa.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+}
