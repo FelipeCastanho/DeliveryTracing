68465375002bca6276c6eaf0f6b6cd351852f3b6
LuisCastanho <luis95castanho@gmail.com>
2017-10-16 10:22:07 -0500

    Mapa terminado

--- app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/MainActivity.java
@@ -11,32 +11,51 @@ import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity{
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = null;
+    Map<String, Object> map = null;
+    String estado = "";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         myRef.setValue("Hello, World!");
     }
 
     public void buttonTapped(View view) {
-
+        boolean change = true;
+        String referencia = "";
         int numId = view.getId();
         String id = view.getResources().getResourceEntryName(numId);
         if(id.equals("rastrear")){
             EditText et1 = (EditText)findViewById(R.id.codigo);
             String codigo = et1.getText().toString();
-            Intent newActivity = new Intent(MainActivity.this,RastreoActivity.class);
-                    //.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);//Con esta linea se mueren las actividades (No la uso aqui porque no es necesario, solo sera necesario despues de iniciar sesion)
-            newActivity.putExtra("codigo", codigo);
-            startActivity(newActivity );
+            referencia = buscarPedido(codigo);
+            if(!estado.equals("activo") && !referencia.equals("")){
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                "Su pedido todavía se encuentra en proceso", Toast.LENGTH_SHORT);
+                toast1.show();
+            }
+           else if(!referencia.equals("")){
+                Intent newActivity = new Intent(MainActivity.this,RastreoActivity.class);
+                newActivity.putExtra("referencia", referencia);
+                startActivity(newActivity );
+            }
         }
         else if(id.equals("login")){
             Intent newActivity = new Intent(MainActivity.this,LoginActivity.class);
@@ -45,4 +64,89 @@ public class MainActivity extends AppCompatActivity{
         }
 
     }
+
+    protected void onStart() {
+        super.onStart();
+        myRef = database.getReference("pedidos/");
+        ValueEventListener pedidoListener = new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                map = (Map<String, Object>) dataSnapshot.getValue();
+            }
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+        myRef.addValueEventListener(pedidoListener);
+    }
+
+    private String buscarPedido(String codigo) {
+        String respuesta = "";
+        try{
+            int contador = 0;
+            Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
+            Map.Entry<String, Object> entry= null;
+            String n=null;
+            Map<String, Object> mapPedidos = null;
+            Map<String, Object> mapPedido = null;
+            while (it.hasNext()) {
+                contador++;
+                entry= it.next();
+                n = entry.getKey();
+                mapPedidos = (Map<String, Object>) entry.getValue();
+                Iterator<Map.Entry<String, Object>> itPedidos = map.entrySet().iterator();
+                Map.Entry<String, Object> entryPedidos = itPedidos.next();
+                if(entryPedidos.getKey().equals(codigo)){
+                    mapPedido = (Map<String, Object>) entryPedidos.getValue();
+                    Iterator<Map.Entry<String, Object>> itPedido = mapPedido.entrySet().iterator();
+                    Map.Entry<String, Object> entryPedido = itPedido.next();
+                    respuesta = entryPedido.getKey();
+                    entryPedido = itPedido.next();
+                   estado = (String)entryPedido.getValue();
+
+                }
+            }
+        }catch (Exception ex){
+            Toast toast1 =
+                    Toast.makeText(getApplicationContext(),
+                            "No se ha encontrado el pedido", Toast.LENGTH_SHORT);
+            toast1.show();
+        }
+        if(respuesta.equals("")){
+            Toast toast1 =
+                    Toast.makeText(getApplicationContext(),
+                            "No se ha encontrado el pedido", Toast.LENGTH_SHORT);
+            toast1.show();
+        }
+        return respuesta;
+    }
+
 }
+/* ValueEventListener pedidoListener = new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int contador = 0;
+                String valor = "";
+                try{
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    Iterator<Map.Entry<String, Object>> it = map.entrySet().iterator();
+                    Map.Entry<String, Object> entry= null;
+                    String n=null;
+                    while (it.hasNext()) {
+                        entry= it.next();
+                        n = entry.getKey(); //clave
+                        contador++;
+                        if(contador == 1) url = "empleados/"+n;
+                    }
+                }catch (Exception ex){
+                    change = false;
+                    Toast toast1 =
+                            Toast.makeText(getApplicationContext(),
+                                    "No se ha encontrado el pedido", Toast.LENGTH_SHORT);
+                    toast1.show();
+                }
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        myRef.addValueEventListener(pedidoListener);*/
--- app/src/main/java/com/example/yoyo/deliverytracing/RastreoActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/RastreoActivity.java
@@ -1,12 +1,23 @@
 package com.example.yoyo.deliverytracing;
 
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.nfc.Tag;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -18,77 +29,84 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-public class RastreoActivity extends AppCompatActivity {
+public class RastreoActivity extends AppCompatActivity  implements OnMapReadyCallback,ValueEventListener {
     String codigo;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = null;
+    DatabaseReference myRefEmpleado = null;
+    private DatabaseReference mLatitud;
+    private DatabaseReference mLongitud;
+    private GoogleMap mMap;
+    private Marker marcador;
+    Location location = new Location(LocationManager.GPS_PROVIDER);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_rastreo);
-        codigo = getIntent().getExtras().getString("codigo");
-        myRef = database.getReference("pedidos/"+codigo);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        String referencia = getIntent().getExtras().getString("referencia");
+        myRefEmpleado = database.getReference("empleados/"+referencia);
+        mLatitud = myRefEmpleado.child("latitud");
+        mLongitud = myRefEmpleado.child("longitud");
+        mLatitud.addValueEventListener(this);
+        mLongitud.addValueEventListener(this);
     }
 
+    public void onMapReady(GoogleMap googleMap) {
 
+        mMap = googleMap;
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        ValueEventListener pedidoListener = new ValueEventListener() {
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                //String key;
-                //Object value;
-                //FirebaseDatabase database = FirebaseDatabase.getInstance();
-                //DatabaseReference myRef = database.getReference("message");
-                //Pedidos post = dataSnapshot.getValue(Pedidos.class);
-                //myRef.setValue(post.toString());
-
-                DatabaseReference myRef = database.getReference("message");
-
-                String valor = "";
-                try{
-                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
-                    Iterator <Map.Entry<String, Object>> it = map.entrySet().iterator();
-                    Map.Entry<String, Object> entry= null;
-
-                    String n=null;
-                    while (it.hasNext()) {
-                        entry= it.next();
-                        n = entry.getKey(); //clave
-                        String dato = "";
-                        boolean opcion = false;
-                        try{
-                            dato = (String) entry.getValue();
-                        }
-                        catch (Exception ex){
-                            opcion =  (boolean)entry.getValue();
-                        }
-                        valor += n;
-                        if(!dato.equals("")){
-                            valor += " "+dato + " - ";
-                        }
-                        else{
-                            valor += " "+opcion + " - ";
-                        }
-
-                    }
-
-                    myRef.setValue(valor);
-                }catch (Exception ex){
-
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-        myRef.addValueEventListener(pedidoListener);
     }
 
+    public void agregarMarcador(double latitud, double longitud) {
+        LatLng coordenadas = new LatLng(latitud, longitud);
+        if (marcador != null) {
+            marcador.remove();
+        }
+        //.icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_launcher))
+        marcador = mMap.addMarker(new MarkerOptions().position(coordenadas).title("Mi ubicación"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordenadas, 16));
+        Toast toast1 =
+                Toast.makeText(getApplicationContext(),
+                        "A", Toast.LENGTH_SHORT);
+        toast1.show();
+    }
+
+    private void actualizarUbicacion(Location location) {
+        if (location != null) {
+            double latitud = location.getLatitude();
+            double longitud = location.getLongitude();
+            agregarMarcador(latitud, longitud);
+        }
+    }
+
+    public void onDataChange(DataSnapshot dataSnapshot) {
+        if(dataSnapshot.getValue(Double.class) != null){
+            String key = dataSnapshot.getKey();
+            if(key.equals("latitud")){
+                Double latitud = dataSnapshot.getValue(Double.class);
+                location.setLatitude(latitud);
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                latitud+"", Toast.LENGTH_SHORT);
+                toast1.show();
+            }else if(key.equals("longitud")){
+                Double longitud = dataSnapshot.getValue(Double.class);
+                location.setLongitude(longitud);
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                longitud+"", Toast.LENGTH_SHORT);
+                toast1.show();
+            }
+            actualizarUbicacion(location);
+        }
+    }
+
+    public void onCancelled(DatabaseError databaseError) {
+
+    }
 
 }
