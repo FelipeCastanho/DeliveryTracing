af46dc66372efb302051336c6dc26c86f0a03761
konaizumi <alejita-81@hotmail.com>
2017-10-19 17:14:38 -0500

    Vista empleado

--- app/src/main/java/com/example/yoyo/deliverytracing/EmpleadoActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/EmpleadoActivity.java
@@ -2,12 +2,28 @@ package com.example.yoyo.deliverytracing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 public class EmpleadoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_empleado);
+
+        TextView titulo = (TextView) findViewById(R.id.textSaludo);
+
+        String nombreCompleto = getIntent().getExtras().getString("nombreUsuario");
+        String[] nombres = nombreCompleto.split(" ");
+
+        String palabra = "¡Hola ";
+        char[] saludo = palabra.concat(nombres[0]+"!").toCharArray();
+        titulo.setText(saludo,0,saludo.length);
+    }
+
+    public void buttonTapped(View view) {
     }
 }
