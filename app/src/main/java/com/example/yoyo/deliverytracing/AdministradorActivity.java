a903fbfb4df12cc33708987b884ea10bbdf3c902
konaizumi <alejita-81@hotmail.com>
2017-10-19 17:22:04 -0500

    Margin logout vistas

--- app/src/main/java/com/example/yoyo/deliverytracing/AdministradorActivity.java
+++ app/src/main/java/com/example/yoyo/deliverytracing/AdministradorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AdministradorActivity extends AppCompatActivity {
 
@@ -14,15 +15,20 @@ public class AdministradorActivity extends AppCompatActivity {
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_administrador);
         TextView titulo = (TextView) findViewById(R.id.textSaludo);
-
         String nombreCompleto = getIntent().getExtras().getString("nombreUsuario");
         String[] nombres = nombreCompleto.split(" ");
-
         String palabra = "¡Hola ";
         char[] saludo = palabra.concat(nombres[0]+"!").toCharArray();
         titulo.setText(saludo,0,saludo.length);
     }
 
     public void buttonTapped(View view) {
+        int numId = view.getId();
+        String id = view.getResources().getResourceEntryName(numId);
+
+        if(id.equals("imgPedido")) {
+            Toast t = Toast.makeText(getApplicationContext(), "apachurrado pedido", Toast.LENGTH_SHORT);
+            t.show();
+        }
     }
 }
